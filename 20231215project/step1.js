2df9cbe911d40bbbe4ea85cb863635abad993950
rlathddk <abc777z@naver.com>
2023-12-15 14:59:38 +0900

    20231215

---
+++ 20231215project/step1.js
@@ -0,0 +1,90 @@
+console.log( 'js열림');
+
+
+const 이름목록 = [ ]; 
+const 팔굽혀펴기목록 = [ ]; 
+const 윗몸일으키기목록 = [ ]; 
+const 턱걸이목록 = [ ]; 
+ 
+
+
+// 1. 등록 함수 선언
+function 등록함수(){ console.log('등록함수() 실행');
+    //[1.입력] 
+    const 이름 = document.querySelector('#이름').value; 
+    const 팔굽혀펴기 = document.querySelector('#팔굽혀펴기').value;
+    const 윗몸일으키기 = document.querySelector('#윗몸일으키기').value;
+    const 턱걸이 = document.querySelector('#턱걸이').value; 
+    // const 총점 = document.querySelector('#총점').value; console.log( 총점 );
+    // const 등급 = document.querySelector('#등급').value; console.log( 등급 );
+
+
+    //[2.처리] 
+    이름목록.push( 이름 );  
+    팔굽혀펴기목록.push( 팔굽혀펴기); 
+    윗몸일으키기목록.push( 윗몸일으키기 );
+    턱걸이목록.push( 턱걸이 );
+    // 총점목록.push( 총점 );
+
+    console.log( 이름 ); console.log( 팔굽혀펴기 );console.log( 윗몸일으키기 );console.log( 턱걸이 );
+    document.querySelector('#이름,#이름,#윗몸일으키기,#턱걸이').value = ``; alert('등록 성공');
+    
+     //[3.출력]
+
+    출력함수();
+}
+/* 어떻게 로직을 정해야 등급이 나올까 */
+
+function 등급(test){
+
+let result = ""
+let 총점 = Number(팔굽혀펴기목록[test])+Number(윗몸일으키기목록[test])+Number(턱걸이목록[test])
+
+if (총점 >=130) {result= "A"}
+else if (총점 >=120) {result= "B"}
+else if (총점 >=110) {result= "C"}
+else if (총점 >=100) {result= "D"}
+else {result= "fail"}
+
+
+return result;
+
+};
+// 삭제함수
+
+function 삭제함수( 삭제할인덱스 ){      //f start
+    console.log("삭제함수실행");
+
+    이름목록.splice(삭제할인덱스 , 1);
+    팔굽혀펴기목록.splice(삭제할인덱스 , 1);
+    윗몸일으키기목록.splice(삭제할인덱스 , 1);
+    턱걸이목록.splice(삭제할인덱스 , 1);
+
+
+    출력함수()
+
+}   //f end
+
+
+// 출력함수
+
+function 출력함수(){
+    console.log('출력함수실행');
+    //const contentOutput = document.querySelector('#contentOutput')
+    let html = '';
+        for( let i = 0; i<이름목록.length; i++){
+            html +=  `<div class="output">        <!-- JS 출력함수() 부분 가이드 -->
+                        <div class="name">${이름목록[i]}</div>
+                        <div class="list_1">${팔굽혀펴기목록[i]}</div>
+                        <div class="list_2">${윗몸일으키기목록[i]}</div>
+                        <div class="list_3">${턱걸이목록[i]}</div>
+                        <div class="total">${Number(팔굽혀펴기목록[i])+Number(윗몸일으키기목록[i])+Number(턱걸이목록[i])}</div>
+                        <div class="grade">${등급(i)}</div>
+                        <div class="butn"><input onclick="삭제함수(${i})" type="button" value="삭제"></div>
+                    </div>`
+
+    }
+    const contentOutput = document.querySelector('#contentOutput')
+
+    contentOutput.innerHTML=html;
+}
--- day15/step4.js
+++ day15/step4.js
@@ -1,65 +1,60 @@
-console.log('js열림')
+console.log( 'js열림');
 
-const dateArray = [];
-const listArray = [];
-const moneyArray = [];
+const 날짜목록 = [ ]; 
+const 항목목록 = [ ]; 
+const 금액목록 = [ ]; 
 
-function 등록함수(){
-    console.log('등록함수() 실행')
-    // [1. 입력]
-    let date = document.querySelector('#date').value;
-    let list = document.querySelector('#list').value;
-    let money = document.querySelector('#money').value;
-
-
-    // [2. 처리]
-    dateArray.push(date);
-    dateArray.push(list);
-    dateArray.push(money);
-    console.log(date);
-    console.log(list); 
-    console.log(money)
-
-    // [3. 출력]
-    document.querySelector('#date').value = ``; 
-    document.querySelector('#list').value = ``; 
-    document.querySelector('#money').value = ``; alert('등록 성공');
-
+// 1. 등록 함수 선언 
+function 등록함수(){ console.log('등록함수() 실행');
+    //[1.입력] 
+    const date = document.querySelector('#date').value;   console.log( date );
+    const list = document.querySelector('#list').value;   console.log( list );
+    const money = document.querySelector('#money').value;   console.log( money );
+    //[2.처리] 
+    날짜목록.push( date );  
+    항목목록.push( list ); 
+    금액목록.push( money );
+    console.log( 날짜목록 ); console.log( 항목목록 );console.log( 금액목록 );
+    //
+     document.querySelector('#date,#list,#money').value = ``; alert('등록 성공');
+    //[3.출력]
     출력함수();
-
-}
-
-function 삭제함수(삭제인덱스){
-    console.log('삭제인덱스() 실행' + 삭제인덱스);
+} // f end 
 
 
-    // [1. 입력
-
-    // [2. 처리]
-    dateArray.spilce(삭제인덱스, 1);
-    listArray.spilce(삭제인덱스, 1);
-    moneyArray.spilce(삭제인덱스, 1);
-
-    // [3. 출력]
+// 2. 삭제 함수
+function 삭제함수( 삭제할인덱스 ){
+    console.log('삭제함수() 실행' + 삭제할인덱스);
+    //[1.입력] X
+    //[2.처리] 
+    날짜목록.splice( 삭제할인덱스 , 1 );   
+    항목목록.splice( 삭제할인덱스 , 1 );  
+    금액목록.splice( 삭제할인덱스 , 1 );  
+    //[3.출력]
     출력함수();
+} // f end 
 
-
-}
+// 4. 출력 함수 
 function 출력함수(){
-    // 어디에
-    const Bottom = document.querySelector('#Bottom');
 
-    let html = '';
-    // 배열 꺼내기
-    for(let i=0; i<dateArray.length; i++){
-        html += ` <div class="dateIn">
-                    <p>${dateArray[i]}</p>
-                    <p>${listArray[i]}</p>
-                    <p>${moneyArray[i]}</p>
-                    <p><button onclick="삭제함수(${i})" value="삭제">삭제</button></p>
-                </div>`
-    }
-
-  // 3. [대입]
-  Bottom.innerHTML = html;
+    // 1.[어디에]
+    const date_box = document.querySelector('#date_box');
+    // 2.[무엇을] 
+    let html = ``;
+        for( let i = 0 ; i<날짜목록.length ; i++ ){ 
+           
+            html += `
+                    <div id="date_box_inner">
+                        <p>${날짜목록[i]}</p>
+                        <p>${항목목록[i]}</p>
+                        <p>${금액목록[i]}</p>
+                        <input onclick="삭제함수( ${ i } )" type="button" value="삭제">
+                    </div>
+                    `
+        } // f end 
+    // 3.[대입]
+    date_box.innerHTML=html;
+    document.querySelector('#date').value=''
+    document.querySelector('#list').value=''
+    document.querySelector('#money').value=''
 }
